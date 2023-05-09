4cface180b000c855f2165af317c361dcb42acdb
teban <esferreres@gmail.com>
2023-05-09 09:23:22 -0300

    nueva hoja de estilos

--- getDataForm.js
+++
@@ -1,9 +0,0 @@
-const todoForm = document.getElementById('todo_form');
-
-todoForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const name = todoForm['todo_name'].value;
-    const url = todoForm['todo_url'].value;
-    const description = todoForm['todo_description'].value;
-    console.log(name, url, description);
-});
